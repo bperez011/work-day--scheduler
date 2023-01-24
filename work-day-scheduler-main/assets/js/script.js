110ebece1bbf1b6260840a26cb6999f658fe03bc
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-01-23 21:21:47 -0600

    second commit

--- work-day-scheduler-main/assets/js/script.js
+++ work-day-scheduler-main/assets/js/script.js
@@ -1,55 +1,86 @@
-$(document).ready(function() {
-    console.log("Ready!");
-});
-    //Display current date and time (Moment.js)
-    let now = moment().format("dddd, MMMM Do YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = now;
-    let currentHour = moment().format("HH");
-
-    //Past, present, and future functionality -
-    //Compare timeblock time with current time to determine whether
-    //activity is in the past, present, or future 
-    $(".time-div").each(function() {
-        var timeDiv = $(this).attr("id").split("-")[1];
-
-        if (currentHour == timeDiv) {
-            $(this).addClass("present");
-            $(this).children(".description").addClass("present");
-   
-        } else if (currentHour < timeDiv) {
-            $(this).removeClass("present");
-            $(this).addClass("future");
-
-        } else if (currentHour > timeDiv) {
-            $(this).removeClass("future");
-            $(this).addClass("past");
+$(document).ready(function () {
+    var init = false;
+    var now = moment().format('LLLL');
+    let now24 = moment().format('H'); 
+    let now12 = moment().format('h'); 
+    if (init) {
+        now24 = 13;
+        now12 = 1;
+    }
+    const currentDate = $('#currentDay');
+    currentDate.text(now);
+    var cal = new Date(); 
+    var currentHour = cal.getHours(); 
+    for (var i = 8; i < 18; i++) {
+        if (i < currentHour) {
+            document.getElementById(i.toString()).classList.add('past');
+        } else if (i === currentHour) {
+            document.getElementById(i.toString()).classList.add('present');
+        } else if (i > currentHour) {
+            document.getElementById(i.toString()).classList.add('future');
         }
-    });
+    }
+     {
+        $('.saveBtn').click(function () {
+            var hr6 = document.getElementById('6').value;
+			localStorage.setItem('text6', hr6);
 
-    //Save data to local storage
-    $(".saveBtn").click(function (event) {
-        event.preventDefault();
-        var value = $(this).siblings(".time-block").val();
-        var time = $(this).parent().attr("id").split("-")[1];
-        localStorage.setItem(time,value);
-    });
+			var hr7 = document.getElementById('7').value;
+			localStorage.setItem('text7', hr7);
 
-    //Retrieve data from local storage 
-    $("#hour-09 .time-block").val(localStorage.getItem("09"));
-    $("#hour-10 .time-block").val(localStorage.getItem("10"));
-    $("#hour-11 .time-block").val(localStorage.getItem("11"));
-    $("#hour-12 .time-block").val(localStorage.getItem("12"));
-    $("#hour-13 .time-block").val(localStorage.getItem("13"));
-    $("#hour-14 .time-block").val(localStorage.getItem("14"));
-    $("#hour-15 .time-block").val(localStorage.getItem("15"));
-    $("#hour-16 .time-block").val(localStorage.getItem("16"));
-    $("#hour-17 .time-block").val(localStorage.getItem("17"));
+			var hr8 = document.getElementById('8').value;
+			localStorage.setItem('text8', hr8);
 
-     
-    //Clear button function for clearing content and local storage
-     $("#clearFieldsBtn").click(function(event) {
-        event.preventDefault;
-        $("textArea").val("");
-        localStorage.clear();
-    });
+			var hr9 = document.getElementById('9').value;
+			localStorage.setItem('text9', hr9);
+
+			var hr10 = document.getElementById('10').value;
+			localStorage.setItem('text10', hr10);
+
+			var hr11 = document.getElementById('11').value;
+			localStorage.setItem('text11', hr11);
+
+			var hr12 = document.getElementById('12').value;
+			localStorage.setItem('text12', hr12);
+
+			var hr1 = document.getElementById('1').value;
+			localStorage.setItem('text1', hr1);
+
+			var hr2 = document.getElementById('2').value;
+			localStorage.setItem('text2', hr2);
+
+			var hr3= document.getElementById('3').value;
+			localStorage.setItem('text3', hr3);
+            
+        });
+     }
+     var saved6 = localStorage.getItem('text6');
+     document.getElementById('6').value = saved6;
+
+     var saved7 = localStorage.getItem('text7');
+     document.getElementById('7').value = saved7;
+
+     var saved8 = localStorage.getItem('text8');
+     document.getElementById('8').value = saved8;
+
+     var saved9 = localStorage.getItem('text9');
+     document.getElementById('9').value = saved9;
+
+     var saved10 = localStorage.getItem('text10');
+     document.getElementById('10').value = saved10;
+
+     var saved11 = localStorage.getItem('text11');
+     document.getElementById('11').value = saved11;
+
+     var saved12 = localStorage.getItem('text12');
+     document.getElementById('12').value = saved12;
+
+     var saved1 = localStorage.getItem('text1');
+     document.getElementById('1').value = saved1;
+
+     var saved2 = localStorage.getItem('text2');
+     document.getElementById('2').value = saved2;
+
+     var saved3 = localStorage.getItem('text3');
+     document.getElementById('3').value = saved3;
+});
